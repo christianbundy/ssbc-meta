f9d7a0229392f19e0352c7448d98ff2c689afbed
Christian Bundy <christianbundy@fraction.io>
2018-11-01 10:20:24 -0700

    Pull up to 200 repos

--- _docs/update.sh
+++ _docs/update.sh
@@ -1,1 +1,2 @@
 for name in $(curl -s 'https://api.github.com/orgs/ssbc/repos?per_page=100' | jq -r '.[].name'); do meta project add $name git@github.com:ssbc/$name.git; done
+for name in $(curl -s 'https://api.github.com/orgs/ssbc/repos?per_page=100&page=2' | jq -r '.[].name'); do meta project add $name git@github.com:ssbc/$name.git; done
